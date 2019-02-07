3680c657bd3bc6b85c991c4c737bb96f5c35cbbd
Vimean Sam <vimeansam2017@gmail.com>
2019-02-06 20:28:16 -0800

    finish functions

--- node/functions.js
+++ node/functions.js
@@ -15,5 +15,11 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
-
+const myArrow = (y) => y+1;
+console.log(myArrow(8));
 // make a function that returns a function
+const functionFactory = () => () => console.log('Function within a function');
+const a = functionFactory();
+a();
+const another = functionFactory();
+another();

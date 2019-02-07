9de8e9beccd49cc65d0056c167ed6bfe73467f00
Vimean Sam <vimeansam2017@gmail.com>
2019-02-06 20:38:16 -0800

    array operations

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,28 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
+const test = [9, 3, 4];
+console.log(test);
+test.forEach((n) => {
+    console.log(n);
+});
 
 // use map
-
+const newArray = test.map(i => i+1);
+console.log(newArray);
 // use pop
-
+//remove the last element
+newArray.pop();
+console.log(newArray);
 // use push
-
+newArray.push(6);
+console.log(newArray);
 // use shift
-
+//pop front
+newArray.shift();
+console.log(newArray);
 // use unshift
-
+//push front
+newArray.unshift(5);
+console.log(newArray);
 // use filter

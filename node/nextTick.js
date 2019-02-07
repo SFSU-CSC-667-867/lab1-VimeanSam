3b72a669f9bf20beee58a0c544ccbf2578803b4f
Vimean Sam <vimeansam2017@gmail.com>
2019-02-06 21:07:04 -0800

    lab 1 finished

--- node/nextTick.js
+++ node/nextTick.js
@@ -1,4 +1,8 @@
-setImmediate(() => console.log('Now'));
 process.nextTick(() => console.log('Hello'));
-
+setImmediate(() => console.log('Now'));
+setImmediate(() => console.log('Now'));
+setImmediate(() => console.log('Now'));
+setImmediate(() => console.log('Now'));
+setImmediate(() => console.log('Now'));
 console.log('world');
+console.log('world 2');
